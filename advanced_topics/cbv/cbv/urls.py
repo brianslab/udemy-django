ca3302aa219677feec13b50a7fcb11f4f91d3843
brianslab <bdkuhn602@gmail.com>
2023-05-15 03:54:43 -0700

    implement basic class-based view

--- advanced_topics/cbv/cbv/urls.py
+++ advanced_topics/cbv/cbv/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from basic_app import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.CBView.as_view()),
     path('admin/', admin.site.urls),
 ]
