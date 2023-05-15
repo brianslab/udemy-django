9cf3ee97c526fca3b259871ca3df4e08e34197b0
brianslab <bdkuhn602@gmail.com>
2023-05-14 23:56:09 -0700

    render table of access records

--- django_l1/django_l1/first_app/views.py
+++ django_l1/django_l1/first_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from first_app.models import Webpage, Topic, AccessRecord
+
 # Create your views here.
 
 
 def index(request):
-    my_dictionary = {
-        'insert_me': "Hello from first_app/views.py"
-    }
-    return render(request, 'django_l1/index.html', context=my_dictionary)
+    webpages = AccessRecord.objects.order_by('date')
+    dates = {'access_records': webpages}
+
+    return render(request, 'first_app/index.html', context=dates)
