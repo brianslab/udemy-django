bc7c2e219d6a668043e6a1ea8e54b66cf3feea32
brianslab <bdkuhn602@gmail.com>
2023-05-15 04:13:38 -0700

    use context dictionary with cbv

--- advanced_topics/cbv/basic_app/views.py
+++ advanced_topics/cbv/basic_app/views.py
@@ -4,3 +4,8 @@ from django.views.generic import View, TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["injectme"] = "injected from basic_app/views.py"
+        return context
