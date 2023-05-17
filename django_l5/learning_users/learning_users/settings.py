71f24c1e3f39d4b9bf77d72c0df8f0218ecf7bd4
brianslab <bdkuhn602@gmail.com>
2023-05-17 05:38:19 -0700

    fix login, profile pic, and redirect bugs

--- django_l5/learning_users/learning_users/settings.py
+++ django_l5/learning_users/learning_users/settings.py
@@ -128,10 +128,10 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [BASE_DIR / 'media']
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Other
-LOGIN_URL = 'basic_app/user_login'
+LOGIN_URL = 'basic_app/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
