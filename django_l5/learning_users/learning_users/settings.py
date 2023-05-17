ea3e32b6b12fe368de93c8fc90ed156d5125b5c0
brianslab <bdkuhn602@gmail.com>
2023-05-17 04:36:03 -0700

    create login template

--- django_l5/learning_users/learning_users/settings.py
+++ django_l5/learning_users/learning_users/settings.py
@@ -130,6 +130,9 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = [BASE_DIR / 'media']
 
+# Other
+LOGIN_URL = 'basic_app/user_login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
