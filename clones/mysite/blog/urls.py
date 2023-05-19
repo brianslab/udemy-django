cb86d52d24932cabd1788af645df183e859ded7f
brianslab <bdkuhn602@gmail.com>
2023-05-19 01:06:58 -0700

    create comment FBVs

--- clones/mysite/blog/urls.py
+++ clones/mysite/blog/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     path('drafts/', views.DraftListView.as_view(), name='drafts'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post,
+         name='add_comment_to_post'),
+    path('comment/<int:pk>/approve', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/delete', views.comment_remove, name='comment_delete'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 ]
